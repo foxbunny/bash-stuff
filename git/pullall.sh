ff5f35727285383522f88491fb2e101af4d09c69
Branko Vukelic <branko@outernet.is>
2015-11-26 10:46:15 +0100

    Fixed issue with escape sequence in FAILED msg
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- git/pullall.sh
+++ git/pullall.sh
@@ -6,6 +6,6 @@ for dir in $(ls | egrep "$filter" 2>/dev/null); do
     if [ -d "$dir" ] && [ -d "$dir/.git" ]
     then
         echo -ne "\e[32mChecking $dir\e[0m "
-        (cd $dir ; git fetch 2>&1 > /dev/null ; git pull 2>&1 || echo -e "\e[31mFAILED\e\[0m")
+        (cd $dir ; git fetch 2>&1 > /dev/null ; git pull 2>&1 || echo -e "\e[31mFAILED\e[0m")
     fi
 done
