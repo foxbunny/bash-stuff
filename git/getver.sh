7d627ec07528e968ad040c2924ce702e6b08ec69
Branko Vukelic <branko@outernet.is>
2016-02-29 14:41:41 +0100

    Show branches in getver
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- git/getver.sh
+++ git/getver.sh
@@ -4,6 +4,6 @@ for d in *
 do
     if [ -d "$d" ] && [ -d "$d/.git" ]
     then
-        echo $d: $(cd "$d" && git log --decorate=short | grep commit | grep '(HEAD' | cut -f2 -d' ')
+        echo $d: $(cd "$d" && git log --decorate=short --pretty=oneline -1 | egrep -o '.* \(.*\)')
     fi
 done
